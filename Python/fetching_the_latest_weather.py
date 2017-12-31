528f4ea8b5838acad14d5751c5344352a0e4a7ec
Ryan Greenlee <ryan.greenlee1@gmail.com>
2017-12-31 11:28:18 -1000

    Completed Fetching the Weather

--- Python/fetching_the_latest_weather.py
+++
@@ -1,8 +0,0 @@
-from requests import get
-import json
-from pprint import pprint
-
-url = 'https://apex.oracle.com/pls/apex/raspberrypi/weatherstation/getlatestmeasurements/505307'
-
-weather = get(url).json()['items']
-pprint(weather)

3fe26185122592aeaaf3c8ddc55388bf568ca7fb
Ryan Greenlee <ryan.greenlee1@gmail.com>
2018-04-14 11:35:35 -0900

    C printed Hello World in loop

---
+++ C/loop/loop.c
@@ -0,0 +1,10 @@
+/* This is a program in C that prints Hello World 5 times in a loop */
+
+#include <stdio.h>
+
+int main(int argc, char *argv[]) {
+	for(int i = 0; i < 5; i++) {
+		printf("Hello World!\n");
+	}
+	return 0;
+}

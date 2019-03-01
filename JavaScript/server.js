30a6887d5527ff92c1c113d2dc94122b2050ec72
Ryan Greenlee <ryan.greenlee1@gmail.com>
2019-02-28 18:01:00 -1000

    added routes to server.js

--- JavaScript/server.js
+++ JavaScript/server.js
@@ -1,7 +1,20 @@
-const express = require('express')
-const app = express()
-const port = 8000
+// server.js
+
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+
+const PORT = 3000;
+
+const indexRouter = require('./routes/index');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.use('/index', indexRouter);
 
 app.get('/', (req, res) => res.send('Hello World!'))
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.listen(PORT, function() {
+	console.log('Server is running on Port', PORT);
+});

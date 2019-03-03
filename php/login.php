007cdabb2e2920ff4def9d1bdb1cab4d6194746a
Ryan Greenlee <ryan.greenlee1@gmail.com>
2019-03-02 17:24:53 -1000

    Added jwt dependencies

--- php/login.php
+++ php/login.php
@@ -1,5 +1,23 @@
 <?php
+// required headers
+header("Access-Control-Allow-Origin: http://localhost/");
+header("Content-Type: application/json; charset=UTF-8");
+header("Access-Control-Allow-Methods: POST");
+header("Access-Control-Max-Age: 3600");
+header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+
+// files needed to connect to database
 require_once "database.php";
+
+// get posted date
+$data = json_decode(file_get_contents("php://input"));
+
+// generate json web token
+include_once 'libs/php-jwt-master/src/BeforeValidException.php';
+include_once 'libs/php-jwt-master/src/ExpiredException.php';
+include_once 'libs/php-jwt-master/sc/SignatureInvalidException.php';
+include_once 'libs/php-jwt-master/src/JWT.php';
+use /Firebase/JWT/JWT;
 
 $sql = "SELECT * FROM users";
 $result = $conn->query($sql);

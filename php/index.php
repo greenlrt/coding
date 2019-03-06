d6d090179e941cf0bd058ea86010fb1be58f700c
Ryan Greenlee <ryan.greenlee1@gmail.com>
2019-03-05 17:33:19 -1000

    can render html file from php

--- php/index.php
+++ php/index.php
@@ -1,5 +1,7 @@
 <?php
-echo "Hello World!<br/>";
+echo "Hello World!<br>";
+
+echo file_get_contents('../HTML/login.html');
 
 include_once "database.php";
 include_once "login.php";
--- php/login.php
+++ php/login.php
@@ -1,11 +1,11 @@
 <?php
-// required headers
+/*// required headers
 header("Access-Control-Allow-Origin: http://localhost/");
 header("Content-Type: application/json; charset=UTF-8");
 header("Access-Control-Allow-Methods: POST");
 header("Access-Control-Max-Age: 3600");
 header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
-
+*/
 // files needed to connect to database
 require_once "database.php";
 

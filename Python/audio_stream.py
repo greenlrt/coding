cf08f57702cfaefbe763871a641f7caf8fc50f4d
Ryan Greenlee <ryan.greenlee1@gmail.com>
2021-07-23 05:32:15 -0900

    added honolulu tower ATC stream

--- Python/audio_stream.py
+++ Python/audio_stream.py
@@ -4,11 +4,12 @@ import requests
 #stream_url = 'https://s1-fmt2.liveatc.net/ksan1_twr?nocache=2021070414143045414'
 stream_url = 'https://s1-fmt2.liveatc.net/khuf_twr'
 #stream_url = 'https://s1-fmt2.liveatc.net/klax_twr'
+stream_url = 'https://s1-fmt2.liveatc.net/phnl1_twr'
 
 r = requests.get(stream_url, stream=True)
 
 #with open('stream.mp3', 'wb') as f:
-with open('stream.wav', 'wb') as f:
+with open('stream2.wav', 'wb') as f:
       try:
           for block in r.iter_content(1024):
               f.write(block)

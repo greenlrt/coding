55fc2aa6e72553e4de7d5ea4b1cac2d2bf747ba7
Ryan Greenlee <ryan.greenlee1@gmail.com>
2022-03-08 12:55:42 -0500

    Move the cube

--- Python/cube.py
+++ Python/cube.py
@@ -83,8 +83,26 @@ def main():
             if event.type == pygame.QUIT:
                 pygame.quit()
                 quit()
+            
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_LEFT:
+                    glTranslatef(-0.5, 0, 0)
+                if event.key == pygame.K_RIGHT:
+                    glTranslatef(0.5, 0, 0)
 
-        glRotatef(1, 3, 1, 1)
+                if event.key == pygame.K_UP:
+                    glTranslatef(0, 1, 0)
+                if event.key == pygame.K_DOWN:
+                    glTranslatef(0, -1, 0)
+
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if event.button == 4:
+                    glTranslate(0, 0, 1.0)
+                
+                if event.button == 5:
+                    glTranslate(0, 0, -1.0)
+
+        #glRotatef(1, 3, 1, 1)
         glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT)
         Cube()
         pygame.display.flip()

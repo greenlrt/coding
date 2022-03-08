5e1ce2854f58d842b4f5abf2272dca0ab3a2ff2c
Ryan Greenlee <ryan.greenlee1@gmail.com>
2022-03-08 13:43:37 -0500

    Need to improve rotating the cube

--- Python/cube.py
+++ Python/cube.py
@@ -2,6 +2,7 @@ import pygame
 from pygame.locals import *
 from OpenGL.GL import *
 from OpenGL.GLU import *
+import math
 
 verticies= (
     (1, -1, -1),
@@ -71,12 +72,20 @@ def Cube():
 
 def main():
     pygame.init()
-    display = (800,600)
+    width = 800
+    height = 600
+    display = (width, height)
     pygame.display.set_mode(display, DOUBLEBUF|OPENGL)
 
     gluPerspective(45, (display[0]/display[1]), 0.1, 50.0)
 
     glTranslatef(0.0, 0.0, -5)
+
+    drag = False
+    dX = 0
+    dy = 0
+    THETA = 0
+    PHI = 0
 
     while True:
         for event in pygame.event.get():
@@ -95,12 +104,32 @@ def main():
                 if event.key == pygame.K_DOWN:
                     glTranslatef(0, -1, 0)
 
+            if event.type == pygame.MOUSEBUTTONUP:
+                if event.button == 1:
+                    drag = False
+
             if event.type == pygame.MOUSEBUTTONDOWN:
+                if event.button == 1:
+                    drag = True
+                    old_x, old_y = pygame.mouse.get_pos()
                 if event.button == 4:
                     glTranslate(0, 0, 1.0)
                 
                 if event.button == 5:
                     glTranslate(0, 0, -1.0)
+
+            if event.type == pygame.MOUSEMOTION:
+                if drag:
+                    #print(pygame.mouse.get_pos())
+                    x, y = pygame.mouse.get_pos()
+                    dX = (x - old_x) * 2 * math.pi / width
+                    dY = (y - old_y) * 2 * math.pi / height
+                    THETA += dX
+                    PHI += dY
+                    old_x = x
+                    old_y = y
+                    glRotatef(THETA, 0, 1, 0)
+                    glRotatef(PHI, 1, 0, 0)
 
         #glRotatef(1, 3, 1, 1)
         glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT)

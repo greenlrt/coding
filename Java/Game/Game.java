19e845b4b508c04b242f4ade24fc2546197427ee
Ryan Greenlee <ryan.greenlee1@gmail.com>
2022-03-09 12:39:20 -0500

    Moved a ball

--- Java/Game/Game.java
+++ Java/Game/Game.java
@@ -9,26 +9,38 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 
 @SuppressWarnings("serial")
-public class Game extends JPanel{
+public class Game extends JPanel {
+
+	int x = 0;
+	int y = 0;
+
+	private void moveBall() {
+		x = x + 1;
+		y = y + 1;
+	}
 
 	@Override
 	public void paint(Graphics g) {
+		super.paint(g);
 		Graphics2D g2d = (Graphics2D) g;
-		g2d.setColor(Color.RED);
-		g2d.fillOval(0, 0, 30, 30);
-		g2d.drawOval(0, 50, 30, 30);
-		g2d.fillRect(50, 0, 30, 30);
-		g2d.drawRect(50, 50, 30, 30);
-
-		g2d.draw(new Ellipse2D.Double(0, 100, 30, 30));
+		g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING,
+				RenderingHints.VALUE_ANTIALIAS_ON);
+		g2d.fillOval(x, y, 30, 30);
 	}
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 		JFrame frame = new JFrame("Mini Tennis");
-		frame.add(new Game());
-		frame.setSize(300, 300);
+		Game game = new Game();
+		frame.add(game);
+		frame.setSize(300, 400);
 		frame.setVisible(true);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+		while (true) {
+			game.moveBall();
+			game.repaint();
+			Thread.sleep(10);
+		}
 	}
 
 }

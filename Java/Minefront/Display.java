4e9837825bbcd2b70e8ac83907f7d1bfea7957a8
Ryan Greenlee <ryan.greenlee1@gmail.com>
2022-03-14 21:27:00 -0400

    Beginning 3D

--- Java/Minefront/Display.java
+++ Java/Minefront/Display.java
@@ -14,7 +14,7 @@ public class Display extends Canvas implements Runnable {
 
 	public static final int WIDTH = 800;
 	public static final int HEIGHT = 600;
-	public static final String TITLE = "Minefront Pre-Alpha 0.01";
+	public static final String TITLE = "Minefront Pre-Alpha 0.02";
 
 	private Thread thread;
 	private Screen screen;
